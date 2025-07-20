965afd94d8debd824fc025a53c266c5e50da190d
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-07-20 10:53:34 -0600

    new file:   .gitignore 	new file:   Procfile 	new file:   app.py 	new file:   requirements.txt 	new file:   resources/pipeline_desempeno.pkl 	new file:   resources/pipeline_desercion.pkl

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+# Rutas a los pipelines
+PIPE_DESMP = os.path.join('resources', 'pipeline_desempeno.pkl')
+PIPE_DESER = os.path.join('resources', 'pipeline_desercion.pkl')
+
+# Cargar los pipelines
+pipe_desempeno = joblib.load(PIPE_DESMP)
+pipe_desercion = joblib.load(PIPE_DESER)
+
+# Ruta de prueba
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({'status': 'API activa'})
+
+# Ruta para predecir DESEMPEÑO DEL ALUMNO
+@app.route('/predict_desempeno', methods=['POST'])
+def predict_desempeno():
+    try:
+        data = request.get_json(force=True)
+        X_input = pd.DataFrame([data])
+        pred = pipe_desempeno.predict(X_input)[0]
+        label_map = {0: 'RENDIMIENTO BAJO', 1: 'RENDIMIENTO ALTO'}
+        return jsonify({'prediction': int(pred), 'interpretation': label_map[pred]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+# Ruta para predecir SE DIO DE BAJA
+@app.route('/predict_desercion', methods=['POST'])
+def predict_desercion():
+    try:
+        data = request.get_json(force=True)
+        X_input = pd.DataFrame([data])
+        pred = pipe_desercion.predict(X_input)[0]
+        label_map = {0: 'PERMANECE', 1: 'BAJA'}
+        return jsonify({'prediction': int(pred), 'interpretation': label_map[pred]})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
